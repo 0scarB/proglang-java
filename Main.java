7964225d0103b648cbfe59d6eba36dea55c5d8c2
0scarB <oscarb@protonmail.com>
2026-05-15 23:39:26 +0200

    Add if-statement and refactor parse-tree-printout to use string builder

--- Main.java
+++ Main.java
@@ -22,7 +22,10 @@ enum TokenType {
     LOR,
     LPAREN,
     RPAREN,
+    LCURLY,
+    RCURLY,
     SEMI,
+    IF,
     PRINT,
 }
 class Token {
@@ -58,9 +61,8 @@ class Tokenizer {
         char c = currChar();
 
         // Skip whitespace
-        while (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+        while (c == ' ' || c == '\t' || c == '\n' || c == '\r')
             c = nextChar();
-        }
 
         switch (c) {
             case 0:
@@ -94,6 +96,12 @@ class Tokenizer {
             case ')':
                 ++pos;
                 return new Token(TokenType.RPAREN);
+            case '{':
+                ++pos;
+                return new Token(TokenType.LCURLY);
+            case '}':
+                ++pos;
+                return new Token(TokenType.RCURLY);
             case ';':
                 ++pos;
                 return new Token(TokenType.SEMI);
@@ -126,7 +134,9 @@ class Tokenizer {
                 if (identifierStop != identifierStart) {
                     pos = identifierStop;
                     String identifier = src.substring(identifierStart, identifierStop);
-                    if (identifier.equals("print")) {
+                    if (identifier.equals("if")) {
+                        return new Token(TokenType.IF);
+                    } else if (identifier.equals("print")) {
                         return new Token(TokenType.PRINT);
                     } else {
                         throw new ParseError("Other identifiers not handled!");
@@ -144,10 +154,34 @@ class Tokenizer {
     }
 }
 
-interface Expr {}
-record IntVal(int val) implements Expr {
+class ParseTreeStringBuilder {
+    public final StringBuilder sb = new StringBuilder();
+    public int indentDepth = 0;
+    public void append(String s) { sb.append(s); }
+    public void append(  char c) { sb.append(c); }
+    public void newline() {
+        sb.append('\n');
+        sb.repeat("    ", indentDepth);
+    }
+    public String toString() { return sb.toString(); }
+}
+abstract class ParseTreeNode {
+    abstract public void buildString(ParseTreeStringBuilder sb);
     public String toString() {
-        return "INT(" + val + ")";
+        ParseTreeStringBuilder sb = new ParseTreeStringBuilder();
+        buildString(sb);
+        return sb.toString();
+    }
+}
+abstract class Expr extends ParseTreeNode {}
+class IntVal extends Expr {
+    public final int val;
+    public IntVal(int val) { this.val = val; }
+    @Override
+    public void buildString(ParseTreeStringBuilder sb) {
+        sb.append("INT(");
+        sb.append(String.valueOf(val));
+        sb.append(')');
     }
 }
 enum BinOp {
@@ -160,42 +194,82 @@ enum BinOp {
     LAND,
     LOR,
 }
-record BinExpr(BinOp op, Expr lhs, Expr rhs) implements Expr {
-    public String toString() {
-        return op.name() + "(" + lhs + " " + rhs + ")";
+class BinExpr extends Expr {
+    public final BinOp op;
+    public final Expr lhs;
+    public final Expr rhs;
+    public BinExpr(BinOp op, Expr lhs, Expr rhs) {
+        this.op  = op;
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+    @Override
+    public void buildString(ParseTreeStringBuilder sb) {
+        sb.append(op.name());
+        sb.append('(');
+        lhs.buildString(sb);
+        sb.append(' ');
+        rhs.buildString(sb);
+        sb.append(')');
     }
 }
-class Stmt {
+abstract class Stmt extends ParseTreeNode {
     public Stmt next = null;
 }
-class SingleExprStmt extends Stmt {
-    public Expr expr;
-}
-class PrintStmt extends SingleExprStmt {
+class PrintStmt extends Stmt {
+    public final Expr expr;
     public PrintStmt(Expr expr) { this.expr = expr; }
-    public String toString() { return "PRINT(" + expr + ")"; }
+    @Override
+    public void buildString(ParseTreeStringBuilder sb) {
+        sb.append("PRINT(");
+        expr.buildString(sb);
+        sb.append(')');
+    }
 }
-class ExprStmt extends SingleExprStmt {
+class ExprStmt extends Stmt {
+    public final Expr expr;
     public ExprStmt(Expr expr) { this.expr = expr; }
-    public String toString() { return "EXPR_STMT(" + expr + ")"; }
+    @Override
+    public void buildString(ParseTreeStringBuilder sb) {
+        sb.append("EXPR_STMT(");
+        expr.buildString(sb);
+        sb.append(')');
+    }
 }
-class Program {
-    public Stmt firstStmt = null;
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
+class StmtList extends ParseTreeNode {
+    public final Stmt firstStmt;
+    public StmtList(Stmt firstStmt) { this.firstStmt = firstStmt; }
+    public void buildString(ParseTreeStringBuilder sb) {
+        boolean newlineBeforeStmt = false;
         for (Stmt stmt = firstStmt; stmt != null; stmt = stmt.next) {
-            sb.append(stmt.toString());
-            sb.append('\n');
+            if (newlineBeforeStmt) sb.newline();
+            stmt.buildString(sb);
+            newlineBeforeStmt = true;
         }
-        return sb.toString();
+    }
+}
+class IfStmt extends Stmt {
+    public final Expr     cond;
+    public final StmtList body;
+    public IfStmt(Expr cond, StmtList body) {
+        this.cond = cond;
+        this.body = body;
+    }
+    @Override
+    public void buildString(ParseTreeStringBuilder sb) {
+        sb.append("IF(");
+        cond.buildString(sb);
+        sb.indentDepth++;
+        sb.newline();
+        body.buildString(sb);
+        sb.indentDepth--;
+        sb.append(')');
     }
 }
 class Parser {
     private final Tokenizer tokenizer;
 
-    public Parser(String src) {
-        tokenizer = new Tokenizer(src);
-    }
+    public Parser(String src) { tokenizer = new Tokenizer(src); }
 
     public Expr parseExpr() throws ParseError {
         Token tok = tokenizer.next();
@@ -217,6 +291,7 @@ class Parser {
         tok = tokenizer.peek();
         if (tok.type == TokenType.RPAREN ||
             tok.type == TokenType.SEMI   ||
+            tok.type == TokenType.LCURLY ||
             tok.type == TokenType.EOF
         ) {
             return innerExpr;
@@ -243,31 +318,59 @@ class Parser {
 
     public Stmt parseStmt() throws ParseError {
         Token tok = tokenizer.peek();
-        Stmt stmt;
-        if (tok.type == TokenType.PRINT) {
+        if (tok.type == TokenType.IF) {
+            tokenizer.next();
+            Expr condition = parseExpr();
+            StmtList body  = parseBlock();
+            return new IfStmt(condition, body);
+        } else if (tok.type == TokenType.PRINT) {
             tokenizer.next();
             Expr expr = parseExpr();
-            stmt = new PrintStmt(expr);
+            Stmt stmt = new PrintStmt(expr);
+            tok = tokenizer.next();
+            if (tok.type != TokenType.SEMI)
+                throw new ParseError("Expected semicolon ';' at end of statement!");
+            return stmt;
         } else {
             Expr expr = parseExpr();
-            stmt = new ExprStmt(expr);
+            Stmt stmt = new ExprStmt(expr);
+            tok = tokenizer.next();
+            if (tok.type != TokenType.SEMI)
+                throw new ParseError("Expected semicolon ';' at end of statement!");
+            return stmt;
         }
-        tok = tokenizer.next();
-        if (tok.type != TokenType.SEMI)
-            throw new ParseError("Expected semicolon ';' at end of statement!");
-        return stmt;
     }
 
-    public Program parse() throws ParseError {
-        Program prog = new Program();
-        Stmt stmt = null;
-        while (tokenizer.peek().type != TokenType.EOF) {
+
+    public StmtList parseStmtList(TokenType endTokType) throws ParseError {
+        Stmt firstStmt = null;
+        Stmt stmt      = null;
+        for (;;) {
+            Token tok = tokenizer.peek();
+            if (tok.type == endTokType) break;
+            if (tok.type == TokenType.EOF)
+                throw new ParseError("Unexpected end of file!");
             Stmt prevStmt = stmt;
             stmt          = parseStmt();
-            if (prog.firstStmt == null) prog.firstStmt = stmt;
-            if (prevStmt       != null) prevStmt.next  = stmt;
+            if (firstStmt == null) firstStmt     = stmt;
+            if (prevStmt  != null) prevStmt.next = stmt;
         }
-        return prog;
+        return new StmtList(firstStmt);
+    }
+
+    public StmtList parseBlock() throws ParseError {
+        Token tok = tokenizer.next();
+        if (tok.type != TokenType.LCURLY)
+            throw new ParseError("Block must start with curly bracket '{'!");
+        StmtList stmtList = parseStmtList(TokenType.RCURLY);
+        tok = tokenizer.next();
+        if (tok.type != TokenType.RCURLY)
+            throw new ParseError("Block must end with curly bracket '}'!");
+        return stmtList;
+    }
+
+    public StmtList parse() throws ParseError {
+        return parseStmtList(TokenType.EOF);
     }
 }
 
@@ -284,12 +387,12 @@ class Interpreter {
     private int boolToInt(boolean b) { return b ? 1 : 0; }
 
     private int evalExpr(Expr expr) throws InterpreterError {
-        if (expr instanceof IntVal(int val)) return val;
+        if (expr instanceof IntVal intVal) return intVal.val;
 
-        if (expr instanceof BinExpr(BinOp op, Expr lhs, Expr rhs)) {
-            int a = evalExpr(lhs);
-            int b = evalExpr(rhs);
-            return switch (op) {
+        if (expr instanceof BinExpr binExpr) {
+            int a = evalExpr(binExpr.lhs);
+            int b = evalExpr(binExpr.rhs);
+            return switch (binExpr.op) {
                 case ADD  -> a + b;
                 case SUB  -> a - b;
                 case MUL  -> a * b;
@@ -308,21 +411,29 @@ class Interpreter {
         throw new InterpreterError("Unexpected expression type!");
     }
 
-    public String interpret() throws ParseError, InterpreterError {
-        Program prog = parser.parse();
-        for (Stmt stmt = prog.firstStmt; stmt != null; stmt = stmt.next) {
+    public void evalStmtList(StmtList stmtList) throws InterpreterError {
+        for (Stmt stmt = stmtList.firstStmt; stmt != null; stmt = stmt.next) {
             switch (stmt) {
+                case IfStmt ifStmt -> {
+                    int condResult = evalExpr(ifStmt.cond);
+                    if (condResult != 0)
+                        evalStmtList(ifStmt.body);
+                }
                 case PrintStmt printStmt -> {
                     int exprResult = evalExpr(printStmt.expr);
                     output.append(exprResult);
                     output.append('\n');
                 }
                 case ExprStmt exprStmt ->
-                    evalExpr(exprStmt.expr);
-                default ->
-                    throw new InterpreterError("Unexpected statement type!");
+                        evalExpr(exprStmt.expr);
+                default -> throw new InterpreterError("Unexpected statement type!");
             }
         }
+    }
+
+    public String interpret() throws ParseError, InterpreterError {
+        StmtList stmtList = parser.parse();
+        evalStmtList(stmtList);
         return output.toString();
     }
 }
@@ -348,6 +459,18 @@ public class Main {
         System.out.println(parser.parse());
         parser = new Parser("print 1 + 2;");
         System.out.println(parser.parse());
+        parser = new Parser(
+                """
+                if 0 {
+                    print 0;
+                }
+                if 1 {
+                    print 1;
+                }
+                if 1+2 {
+                    print 3;
+                }""");
+        System.out.println(parser.parse());
 
         assert new Interpreter("print 1 + 2;").interpret().equals("3\n");
         assert new Interpreter("print 1 + (2*3);").interpret().equals("7\n");
@@ -365,5 +488,14 @@ public class Main {
             "print 1;\n" +
             "print 2;"
         ).interpret().equals("1\n2\n");
+        assert new Interpreter(
+            """
+            if 0 {
+                print 0;
+            }
+            if 1 {
+                print 1;
+            }"""
+        ).interpret().equals("1\n");
     }
 }
